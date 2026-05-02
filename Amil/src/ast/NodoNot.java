305932bbe7395370ae37b22434c0a18dea322171
franbethlana <franbeth@ucm.es>
2026-05-02 10:18:55 +0200

    chequea en operaciones booleanas

--- Amil/src/ast/NodoNot.java
+++ Amil/src/ast/NodoNot.java
@@ -1,6 +1,7 @@
 package ast;
 
 import semantico.TablaSimbolos;
+import semantico.Tipos;
 
 public class NodoNot extends ExpresionUnaria {
 
@@ -16,8 +17,36 @@ public class NodoNot extends ExpresionUnaria {
 
     @Override
     public void chequea(TablaSimbolos ts) {
+        // Chequeamos el nodo hijo con evaluación bottom-up
         if (operando() != null) {
             operando().chequea(ts);
+        }
+
+        // Extraemos el tipo del operando
+        String tipo = (operando() != null && operando().getTipo() != null)
+                ? operando().getTipo()
+                : Tipos.ERROR;
+
+        if (tipo.equals(Tipos.ERROR)) {
+            this.setTipo(Tipos.ERROR);
+            return;
+        }
+
+        // El operando debe ser booleano
+        boolean hayError = false;
+
+        if (!tipo.equals(Tipos.BOOLEANO)) {
+            System.err.println("Error Semántico [" + getFila() + ":" + getColumna() +
+                    "]: Operando de '!' inválido. Se esperaba 'booleano', pero se encontró '" + tipo
+                    + "'.");
+            hayError = true;
+        }
+
+        // Asignación del tipo resultante a la expresión
+        if (hayError) {
+            this.setTipo(Tipos.ERROR);
+        } else {
+            this.setTipo(Tipos.BOOLEANO);
         }
     }
 
--- Amil/src/ast/NodoOr.java
+++ Amil/src/ast/NodoOr.java
@@ -1,6 +1,7 @@
 package ast;
 
 import semantico.TablaSimbolos;
+import semantico.Tipos;
 
 public class NodoOr extends ExpresionBinaria {
     public NodoOr(int fil, int col, Expresion opIzq, Expresion opDer) {
@@ -15,11 +16,50 @@ public class NodoOr extends ExpresionBinaria {
 
     @Override
     public void chequea(TablaSimbolos ts) {
+        // Chequeamos los nodos hijos con evaluación bottom-up
         if (opIzq() != null) {
             opIzq().chequea(ts);
         }
         if (opDer() != null) {
             opDer().chequea(ts);
+        }
+
+        // Extraemos los tipos de los operandos
+        String tipoIzq = (opIzq() != null && opIzq().getTipo() != null)
+                ? opIzq().getTipo()
+                : Tipos.ERROR;
+
+        String tipoDer = (opDer() != null && opDer().getTipo() != null)
+                ? opDer().getTipo()
+                : Tipos.ERROR;
+
+        if (tipoIzq.equals(Tipos.ERROR) || tipoDer.equals(Tipos.ERROR)) {
+            this.setTipo(Tipos.ERROR);
+            return;
+        }
+
+        // Ambos operandos deben ser booleanos
+        boolean hayError = false;
+
+        if (!tipoIzq.equals(Tipos.BOOLEANO)) {
+            System.err.println("Error Semántico [" + getFila() + ":" + getColumna() +
+                    "]: Operando izquierdo de '||' inválido. Se esperaba 'booleano', pero se encontró '" + tipoIzq
+                    + "'.");
+            hayError = true;
+        }
+
+        if (!tipoDer.equals(Tipos.BOOLEANO)) {
+            System.err.println("Error Semántico [" + getFila() + ":" + getColumna() +
+                    "]: Operando derecho de '||' inválido. Se esperaba 'booleano', pero se encontró '" + tipoDer
+                    + "'.");
+            hayError = true;
+        }
+
+        // Asignación del tipo resultante a la expresión
+        if (hayError) {
+            this.setTipo(Tipos.ERROR);
+        } else {
+            this.setTipo(Tipos.BOOLEANO);
         }
     }
 }
